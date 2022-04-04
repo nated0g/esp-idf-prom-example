32f4fc989610b39cc401dddde2ddadcc87bbc72e
nated0g <n.usher87@gmail.com>
2022-04-03 22:49:36 -0700

    Working first example

---
+++ main/esp_rest_main.c
@@ -0,0 +1,57 @@
+/* HTTP Restful API Server Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "esp_vfs_semihost.h"
+#include "esp_vfs_fat.h"
+#include "esp_spiffs.h"
+#include "sdmmc_cmd.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "mdns.h"
+#include "lwip/apps/netbiosns.h"
+#include "protocol_examples_common.h"
+#include "prom.h"
+#include "./metrics.h"
+
+static const char *TAG = "esp-idf-prom-test";
+
+esp_err_t start_rest_server(const char *base_path);
+
+
+prom_histogram_t *test_histogram;
+
+static void init(void) {
+  // Initialize the Default registry
+  prom_collector_registry_default_init();
+
+  test_histogram = prom_collector_registry_must_register_metric(
+    prom_histogram_new(
+      "test_histogram",
+      "histogram under test",
+      prom_histogram_buckets_linear(5.0, 5.0, 2),
+      0,
+      NULL
+    )
+  );
+}
+
+void app_main(void)
+{
+    init();
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_ERROR_CHECK(example_connect());
+    ESP_ERROR_CHECK(start_rest_server(CONFIG_EXAMPLE_WEB_MOUNT_POINT));
+
+}
---
+++ main/rest_server.c
@@ -0,0 +1,127 @@
+/* HTTP Restful API Server
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <fcntl.h>
+#include "esp_http_server.h"
+#include "esp_chip_info.h"
+#include "esp_random.h"
+#include "esp_log.h"
+#include "esp_vfs.h"
+#include "prom.h"
+#include "metrics.h"
+prom_collector_registry_t *PROM_ACTIVE_REGISTRY;
+
+static const char *REST_TAG = "esp-rest";
+#define REST_CHECK(a, str, goto_tag, ...)                                              \
+    do                                                                                 \
+    {                                                                                  \
+        if (!(a))                                                                      \
+        {                                                                              \
+            ESP_LOGE(REST_TAG, "%s(%d): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+            goto goto_tag;                                                             \
+        }                                                                              \
+    } while (0)
+
+#define FILE_PATH_MAX (ESP_VFS_PATH_MAX + 128)
+#define SCRATCH_BUFSIZE (10240)
+
+typedef struct rest_server_context {
+    char base_path[ESP_VFS_PATH_MAX + 1];
+    char scratch[SCRATCH_BUFSIZE];
+} rest_server_context_t;
+
+#define CHECK_FILE_EXTENSION(filename, ext) (strcasecmp(&filename[strlen(filename) - strlen(ext)], ext) == 0)
+
+/* Set HTTP response content type according to file extension */
+static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filepath)
+{
+    const char *type = "text/plain";
+    if (CHECK_FILE_EXTENSION(filepath, ".html")) {
+        type = "text/html";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".js")) {
+        type = "application/javascript";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".css")) {
+        type = "text/css";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".png")) {
+        type = "image/png";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".ico")) {
+        type = "image/x-icon";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".svg")) {
+        type = "text/xml";
+    }
+    return httpd_resp_set_type(req, type);
+}
+
+/* Simple handler for getting system handler */
+static esp_err_t system_info_get_handler(httpd_req_t *req)
+{
+    httpd_resp_set_type(req, "application/json");
+    cJSON *root = cJSON_CreateObject();
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    cJSON_AddStringToObject(root, "version", IDF_VER);
+    cJSON_AddNumberToObject(root, "cores", chip_info.cores);
+    const char *sys_info = cJSON_Print(root);
+    httpd_resp_sendstr(req, sys_info);
+    free((void *)sys_info);
+    cJSON_Delete(root);
+    return ESP_OK;
+}
+
+/* Handler for Prometheus monitoring endpoint */
+static esp_err_t prom_metrics_handler(httpd_req_t *req) {
+    const char *buf = prom_collector_registry_bridge(PROM_ACTIVE_REGISTRY);
+    httpd_resp_set_type(req, "text/plain");
+    httpd_resp_sendstr(req, buf);
+    free((void *)buf);
+    return ESP_OK;
+}
+
+esp_err_t start_rest_server(const char *base_path)
+{
+    PROM_ACTIVE_REGISTRY = PROM_COLLECTOR_REGISTRY_DEFAULT;
+
+    REST_CHECK(base_path, "wrong base path", err);
+    rest_server_context_t *rest_context = calloc(1, sizeof(rest_server_context_t));
+    REST_CHECK(rest_context, "No memory for rest context", err);
+    strlcpy(rest_context->base_path, base_path, sizeof(rest_context->base_path));
+
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.uri_match_fn = httpd_uri_match_wildcard;
+
+    ESP_LOGI(REST_TAG, "Starting HTTP Server");
+    REST_CHECK(httpd_start(&server, &config) == ESP_OK, "Start server failed", err_start);
+
+    /* URI handler for fetching system info */
+    httpd_uri_t system_info_get_uri = {
+        .uri = "/api/v1/system/info",
+        .method = HTTP_GET,
+        .handler = system_info_get_handler,
+        .user_ctx = rest_context
+    };
+    httpd_register_uri_handler(server, &system_info_get_uri);
+
+    /* URI handler Prometheus monitoring endpoint */
+    httpd_uri_t prom_get_uri = {
+        .uri = "/metrics",
+        .method = HTTP_GET,
+        .handler = prom_metrics_handler,
+        .user_ctx = rest_context
+    };
+    httpd_register_uri_handler(server, &prom_get_uri);
+
+
+
+    return ESP_OK;
+err_start:
+    free(rest_context);
+err:
+    return ESP_FAIL;
+}
